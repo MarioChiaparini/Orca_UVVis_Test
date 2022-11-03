a3108a66692318ac771eefb8672ccf53eb0cd6ed
Mario_Chiaparini <45772937+MarioChiaparini@users.noreply.github.com>
2022-11-03 15:15:34 -0300

    Update uvvis.py

--- analysis/uvvis.py
+++ analysis/uvvis.py
@@ -8,14 +8,15 @@ from scipy.signal import find_peaks
 
 string_start = 'ABSORPTION SPECTRUM VIA TRANSITION ELECTRIC DIPOLE MOMENTS'
 string_end = 'ABSORPTION SPECTRUM VIA TRANSITION ELECTRIC DIPOLE MOMENTS'
-
 path = '/home/ABTLUS/mario.neto/Desktop/orca-out/benzene/benzene.out'
+
+found_uv_section =  False
 
 state = []
 energy = []
 intenity = []
 
-def getOrcaData(path):
+def GetOrcaData(path):
     try:
         with open(path, "r") as input_file:
             for line in input_file: 
@@ -24,13 +25,16 @@ def getOrcaData(path):
                     for line in input_file:
                         if string_end in line:
                             break 
-                        if re.research("\d\s{1,}\d"):
-                            state.append(int(line.strip().split()[0]))
-                            energy.append(float(line.strip().split()[1]))
-                            intenity.append(float(line.strip().split()[3]))
+                        if re.search("\d\s{1,}\d", line):
+                            #state.append(line.strip().split()[0])
+                            #print(line.strip().split()[3])
+                            energy.append(line.strip().split()[1])
+                            intenity.append(line.strip().split()[3])
     except IOError:
         print("Error")
         sys.exit(1)
-    return energy, intenity, state
+    return energy, intenity #, state
 
+data = GetOrcaData(path)
+print(data)
 getOrcaData(path=path)
